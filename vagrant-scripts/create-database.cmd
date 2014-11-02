9b56e010cb787265f6382a5bf7b5de60fb299cb9
Dmitry Zaets <dmitry.zaets@gmail.com>
2014-11-02 13:23:48 +0200

    Added create sql script

--- vagrant-scripts/create-database.cmd
+++ vagrant-scripts/create-database.cmd
@@ -3,6 +3,3 @@
 echo Creating database...
 sqlcmd -S .\sqlexpress -i C:\vagrant\db\create.sql
 echo Database is created
-echo Restoring data..
-sqlcmd -S .\sqlexpress -i C:\vagrant\db\seed.sql
-echo Data is restored
